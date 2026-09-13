9b80a567f564efa82a724e48769f5a83e5c37b11
RANN <rann@xyz.dev>
2026-09-13 18:48:26 +0000

    Add redirects

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  transpilePackages: ['xterm', 'xterm-addon-fit', 'xterm-addon-search'],
+  experimental: {
+    appDir: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/boot',
+        permanent: false,
+      },
+    ];
+  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:4000',
